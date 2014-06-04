22a392051416989ce849e3ed1fd5b325dba7ea47
Daniel Mahon <daniel@mahonstudios.com>
2014-06-04 18:20:08 -0400

    wip

---
+++ test/test.js
@@ -0,0 +1,15 @@
+var assert, cli;
+
+assert = require("assert");
+
+cli = require("../lib/cli");
+
+suite("cli", function() {
+  return suite("print", function() {
+    return test("should throw an error when empty options", function() {
+      return assert.throws((function() {
+        return cli.print();
+      }), Error);
+    });
+  });
+});
